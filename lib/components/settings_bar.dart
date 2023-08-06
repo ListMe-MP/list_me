d3f03ff530ec60443b61f910e4018abe4a9e6e2e
uthpalanayanaramanipathirana@gmail.com <uthpalanayanaramanipathirana@gmail.com>
2023-08-06 20:48:32 +0530

    modified settings page

---
+++ lib/components/settings_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class SettingsBar extends StatelessWidget {
+  final IconData icon;
+  final String title;
+
+
+   const SettingsBar({
+    super.key, 
+    required this.icon, 
+    required this.title
+  });
+  
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+                      width: 299,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromRGBO(131, 193, 193, 1),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 4,
+                            offset: Offset(0, 4),
+                            spreadRadius: 0,
+                          ),
+                        ]
+                      ),
+                      
+                      child:  Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Icon(
+                             icon, 
+                              ),
+                          ),
+                          
+                         
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                               title,
+                                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                                ),
+                            ),
+                            Spacer(),
+                          
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(
+                                Ionicons.chevron_forward, 
+                                color: Colors.black,
+                                
+                              ),
+                            ),   
+                        ],
+                      ),
+                    );
+  }
+}
