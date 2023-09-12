4d0bfe4007bab83ff2e8e41e104d7012bd8b2010
Hashiniwelagedara <hashiniwel@gmail.com>
2023-09-13 00:54:03 +0530

    make changes to connect backend

--- lib/screens/check_list.dart
+++ lib/screens/check_list.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:list_me/components/background.dart';
 import 'package:list_me/components/colors.dart';
@@ -10,18 +12,72 @@ import 'package:list_me/screens/Untitled_List_04_page.dart';
 
 import '../utils/navigationMenu.dart';
 import 'create_list.dart';
+import 'package:http/http.dart' as http;
 
-class CheckList extends StatelessWidget {
-   CheckList({super.key});
-  
-  
+class CheckList extends StatefulWidget {
+  @override
+  _CheckListState createState() => _CheckListState();
+}
 
-  List<String> items=[
-    'Untitle list 04',
-    'Untitle list 03',
-    'Untitle list 02',
-    'Untitle list 01',
-  ]; 
+class _CheckListState extends State<CheckList> {
+  List<String> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getTitleFromServer();
+  }
+
+  // Future<void> fetchDataFromServer() async {
+  //   try {         
+  //     // Replace 'YOUR_API_ENDPOINT' with the actual endpoint to fetch data
+  //     final response = await http.get(Uri.parse("http://localhost:2000/api/get_product/"));
+
+  //     if (response.statusCode == 200) {
+  //       final data = json.decode(response.body);
+  //       if (data is List) {
+  //         // Assuming the server returns a list of strings
+  //         setState(() {
+  //           items = List<String>.from(data);
+  //         });
+  //       } else {
+  //         // Handle the response if it's not a list of strings
+  //       }
+  //     } else {
+  //       // Handle errors or non-200 status codes
+  //     }
+  //   } catch (e) {
+  //     // Handle any exceptions that occur during the HTTP request
+  //     print('Error: $e');
+  //   }
+  // }
+
+  Future <List >getTitleFromServer() async {
+    var url= Uri.parse("http://localhost:2000/api/get_product/");
+    late http.Response response; 
+
+    try { 
+      response = await http.get(url);
+      if(response.statusCode==200) {
+
+        Map title = jsonDecode(response.body);
+        List<dynamic> titleName = title ["results"];
+
+        for (var item in titleName) {
+          var id = item['id'];
+          var ltitle = item['ltitle'];
+        }
+      } else {
+        return Future.error("Something gone wrong, ${response.statusCode}");
+      }
+      
+    } catch (e) {
+      return Future.error(e.toString());
+      
+    }
+      return items; 
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +86,8 @@ class CheckList extends StatelessWidget {
       backgroundColor: tc4,
           foregroundColor: tc1,
       ),
-      body: Stack(
+      body: 
+      Stack(
         children: [
           const Background(),
           Container(
