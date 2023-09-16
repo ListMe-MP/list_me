1032c44c0bccb2050b14856ba01cc7a67d9c4a61
uthpalanayanaramanipathirana@gmail.com <uthpalanayanaramanipathirana@gmail.com>
2023-09-16 12:49:26 +0530

    Added api for the feedback textfield

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,71 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use(express.urlencoded({
+//     extended: true
+// }));
+
+// const productList = [];
+
+// app.listen(2000, ()=>{
+//     console.log("Connected to server at 2000");
+// })
+
+// // post api
+
+// app.post("/api/add_product", (req, res)=>{
+//     console.log("Result", req.body);
+
+//     const itemArray = req.body.items.map(item => {
+//         return {
+//             "pname": item,
+//             "pimage": "",
+//             "pprice": "",
+//             "pquantity": "",
+//         }
+//     })
+
+//     const pdata = {
+//         "id": productList.length+1,
+//         "lTitle": req.body.ltitle,
+//         "items": itemArray
+
+//     };
+
+//     productList.push(pdata);
+//     console.log("Final", pdata);
+
+//     res.status(200).send({
+//         "status_code": 200,
+//         "message": "Product added successfully",
+//         "product": pdata
+//     })
+// })
+
+// // get Api
+
+// app.get("/api/get_product",(req,res)=> {
+
+
+//     if (productList.length > 0 ){
+//         res.status(200).send({
+//             'status_code':200,
+//             'products': productList
+//         });
+//     } else {
+//         res.status(200).send({
+//             'status_code':200,
+//             'products': []
+
+//     })
+
+
+// }})
+
+
 const express = require("express");
 
 const app = express();
@@ -9,9 +77,11 @@ app.use(express.urlencoded({
 }));
 
 const productList = [];
+const feedbackData = [];
 
-app.listen(2000, ()=>{
-    console.log("Connected to server at 2000");
+
+app.listen(3000, ()=>{
+    console.log("Connected to server at 3000");
 })
 
 // post api
@@ -43,6 +113,33 @@ app.post("/api/add_product", (req, res)=>{
         "message": "Product added successfully",
         "product": pdata
     })
+})
+
+//post api for feedback
+app.post("/api/add_feedback", (req, res) => {
+  console.log("Request Body:", req.body);
+
+    const pdata = {
+        "id": feedbackData.length+1,
+        // "pfeedback": req.body.pfeedback,
+        
+        "pfeedback": req.body.pfeedback
+
+
+       // "pfeedback": req.body.pdata,
+       
+    };
+
+    feedbackData.push(pdata);
+    console.log("Final", pdata);
+
+    res.status(200).send({
+        "status_code": 200,
+        "message": "Feedback added successfully",
+        "feedback": pdata
+    });
+    
+
 })
 
 // get Api
